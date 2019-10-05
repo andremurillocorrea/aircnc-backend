a9900ffbd39237948dbec283eb7f3e094a2a9642
Andre Correa <andre.murillo.correa@gmail.com>
2019-10-05 20:12:26 -0300

    include session handling

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://andrecorrea:aircnc@aircnc-cvjc7.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 app.use(express.json())
 app.use(routes)
 
