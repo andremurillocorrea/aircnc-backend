e443a5de18dad29d8b4bc4633f7d8bdcb4cca78e
Andre Correa <andre.murillo.correa@gmail.com>
2019-10-06 23:47:03 -0300

    include push notifications via socket

--- src/server.js
+++ src/server.js
@@ -1,19 +1,40 @@
-const express = require('express')
-const routes = require('./routes')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
-const app = express()
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect('mongodb+srv://andrecorrea:aircnc@aircnc-cvjc7.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+});
 
-app.use(cors())
-app.use(express.json())
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
-app.use(routes)
+const connectedUsers = {};
 
-app.listen(3333)
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
